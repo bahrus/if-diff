8dc86c35502a9cff680d7e3c4249d8244cff96a7
bahrus <anderson.bruce.b@gmail.com>
2018-12-09 13:49:02 -0500

    ...

---
+++ test/test_IfDiff.js
@@ -0,0 +1,18 @@
+const xt = require('xtal-test/index');
+const test = require('tape');
+async function customTests(page) {
+    const textContent = await page.$eval('#secondEditor', (c) => c.input);
+    const TapeTestRunner = {
+        test: test
+    };
+    TapeTestRunner.test('testing dev.html', (t) => {
+        t.equal(textContent.data[0].name, 'Harry Potter');
+        t.end();
+    });
+}
+(async () => {
+    await xt.runTests({
+        path: 'demo/dev.iife.html'
+    }, customTests);
+})();
+//# sourceMappingURL=test_IfDiff.js.map
---
+++ test/test_IfDiff.ts
@@ -0,0 +1,22 @@
+import { IXtalTestRunner, IXtalTestRunnerOptions } from 'xtal-test/index.js';
+const xt = require('xtal-test/index') as IXtalTestRunner;
+const test = require('tape');
+import { Page } from "puppeteer"; //typescript
+import { Test } from "tape";
+async function customTests(page: Page) {
+    const textContent = await page.$eval('#secondEditor', (c: any) => c.input);
+    const TapeTestRunner = {
+        test: test
+    } as Test;
+    TapeTestRunner.test('testing dev.html', (t: any) => {
+        t.equal(textContent.data[0].name, 'Harry Potter');
+        t.end();
+    });
+
+}
+
+(async () => {
+    await xt.runTests({
+        path: 'demo/dev.iife.html'
+    }, customTests);
+})();
